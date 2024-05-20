28fc61e8e2f3b4bfb2c5cef34f56b2b9a5d8bc39
Juan Miguel Lugo Parra <juanmi679@gmail.com>
2024-05-19 22:03:19 -0400

    add github workflow to deploy app

---
+++ _nuxt/finalintroexpand_1.92e5df0a.js
@@ -0,0 +1,1 @@
+import"./entry.7751189c.js";const t=""+new URL("finalintroexpand_1.3b335c6c.mp4",import.meta.url).href;export{t as _};
---
+++ _nuxt/gallery.e0bc8837.js
@@ -0,0 +1,1 @@
+import{_ as n}from"./navbar.717b6a9e.js";import{_ as o,c as t,b as a,F as c,a as r,o as s}from"./entry.7751189c.js";import"./nuxt-link.4e279c71.js";const _={},l=r("div",null,null,-1);function m(d,i){const e=n;return s(),t(c,null,[a(e,{style:{"z-index":"1"}}),l],64)}const x=o(_,[["render",m]]);export{x as default};
